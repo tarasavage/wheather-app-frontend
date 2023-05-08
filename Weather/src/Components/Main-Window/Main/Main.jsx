9153928e1de57d6f2bf769da99d309a455d287a9
NazarFedus <simpleprograming7@gmail.com>
2023-05-08 18:56:19 +0300

    create a structure of main content

---
+++ Weather/src/Components/Main-Window/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Main.module.scss'
+import LeftSideForecast from './components/LeftSideForecast/LeftSideForecast'
+import MainContent from './components/MainContent/MainContent'
+
+function Main() {
+  return (
+    <div className={styles.wrapper}>
+      <LeftSideForecast />
+      <MainContent />
+    </div>
+  )
+}
+
+export default Main
---
+++ Weather/src/Components/Main-Window/Main/components/MainContent/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './MainContent.module.scss'
+function MainContent() {
+  return (
+    <div>
+          <h3>kdsfnkdsnfkjsdf</h3>
+    </div>
+  )
+}
+
+export default MainContent
