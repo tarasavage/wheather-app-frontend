c10e29c239b22ee07c2d7646fb37c37cd4e80479
NazarFedus <simpleprograming7@gmail.com>
2023-05-14 11:04:59 +0300

    add full main

--- Weather/src/Pages/Main-Window/Components/Main/components/LeftSideForecast/LeftSideForecast.jsx
+++ Weather/src/Pages/Main-Window/Components/Main/components/LeftSideForecast/LeftSideForecast.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import styles from "./LeftSideForecast.module.scss";
+import { format } from 'date-fns';
 
 import currentweather from "../../../../../../icons/cloudyWithSun.svg";
-import calendar from '../../../../../../icons/calendar.svg'
+import calendar from "../../../../../../icons/calendar.svg";
+import star from '../../../../../../icons/choosen_star.png'
 
-import WeatherCard from '../../ui/WeatherCard/WeatherCard'
+import WeatherCard from "../../ui/WeatherCard/WeatherCard";
+import { translateDay } from "../../Helpers/leftForecast.helper";
 
 const data = {
   id: 1,
@@ -54,10 +57,84 @@ function LeftSideForecast() {
     return `${hours}:${minutes < 10 ? "0" + minutes : minutes}`;
   }
 
+  const weatherData = [
+    {
+      id: 1,
+      date: new Date(2023, 4, 15),
+      temperature: 20,
+      condition: "Sunny",
+      weather_icon: currentweather,
+    },
+    {
+      id: 2,
+      date: new Date(2023, 4, 16),
+      temperature: 18,
+      condition: "Cloudy",
+      weather_icon: currentweather,
+    },
+    {
+      id: 3,
+      date: new Date(2023, 4, 15),
+      temperature: 20,
+      condition: "Sunny",
+      weather_icon: currentweather,
+    },
+    {
+      id: 4,
+      date: new Date(2023, 4, 16),
+      temperature: 18,
+      condition: "Cloudy",
+      weather_icon: currentweather,
+    },
+    {
+      id: 5,
+      date: new Date(2023, 4, 15),
+      temperature: 20,
+      condition: "Sunny",
+      weather_icon: currentweather,
+    },
+    {
+      id: 6,
+      date: new Date(2023, 4, 16),
+      temperature: 18,
+      condition: "Cloudy",
+      weather_icon: currentweather,
+    },
+    {
+      id: 7,
+      date: new Date(2023, 4, 15),
+      temperature: 20,
+      condition: "Sunny",
+      weather_icon: currentweather,
+    },
+    {
+      id: 8,
+      date: new Date(2023, 4, 16),
+      temperature: 18,
+      condition: "Cloudy",
+      weather_icon: currentweather,
+    },
+    {
+      id: 9,
+      date: new Date(2023, 4, 15),
+      temperature: 20,
+      condition: "Sunny",
+      weather_icon: currentweather,
+    },
+    {
+      id: 10,
+      date: new Date(2023, 4, 16),
+      temperature: 18,
+      condition: "Cloudy",
+      weather_icon: currentweather,
+    },
+  ];
+
   return (
     <div className={styles.leftside_wrapper}>
-      <div>
-        <img src="" alt="" />
+      <div className={styles.choosen_wrapper}>
+        <img src={star} alt="star" />
+        <p>У вибраному</p>
       </div>
       <div className={styles.top_title_block}>
         <h3 className={styles.top_title}>{data.city.name}</h3>
@@ -80,10 +157,15 @@ function LeftSideForecast() {
           <p>10-денний прогноз</p>
         </div>
         <div className={styles.ten_main_block}>
-        {Array.from({ length: 10 }, (_, index) => (
-        <WeatherCard key={index} />
-      ))}
-          </div>
+          {weatherData.map((card) => (
+            <WeatherCard
+              key={card.id}
+              day={translateDay(format(card.date, 'EEEE'))}
+              icon={card.weather_icon}
+              temp={card.temperature}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- Weather/src/Pages/Main-Window/Components/Main/ui/WeatherCard/WeatherCard.tsx
+++ Weather/src/Pages/Main-Window/Components/Main/ui/WeatherCard/WeatherCard.tsx
@@ -4,10 +4,12 @@ import styles from './WeatherCard.module.scss'
 const WeatherCard = ({day, icon, temp}) => {
   return (
     <div className={styles.weathercard_wrapper}>
-          <div className={styles.weathercard_day}>{day}</div>
+          <div className={styles.weathercard_day}>
+               <p>{day}</p>
+          </div>
           <div className={styles.weathercard_content}>
                <img src={icon} alt="weather-icon" />
-               <p>{temp}</p>
+               <p>+{temp}</p>
           </div>
     </div>
   )
